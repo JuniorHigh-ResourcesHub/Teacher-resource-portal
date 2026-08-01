79daa3880e39ce183eb34d9760a503fe3e21b9d4
JuniorHigh-ResourcesHub <jdacyon@gmail.com>
2026-08-01 18:46:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -106,3 +106,95 @@ darkModeBtn.addEventListener("click", function(){
     document.body.classList.toggle("dark-mode");
 
 });
+const categoryButtons = document.querySelectorAll(".category-btn");
+const resourceList = document.getElementById("resourceList");
+
+let resources = [
+    {
+        title: "Daily Lesson Plan Template",
+        category: "Lesson Plans",
+        file: "files/DLL_Template.docx"
+    },
+    {
+        title: "School Form 1",
+        category: "School Forms",
+        file: "files/SF1.docx"
+    },
+    {
+        title: "Teacher Template",
+        category: "Templates",
+        file: "files/Template.docx"
+    },
+    {
+        title: "TLE Learning Materials",
+        category: "TLE Resources",
+        file: "files/TLE_Module.pdf"
+    },
+    {
+        title: "Action Research Sample",
+        category: "Research",
+        file: "files/Research.docx"
+    }
+];
+
+
+function displayResources(category="All Resources") {
+
+    resourceList.innerHTML = "";
+
+    let filtered;
+
+    if(category === "All Resources") {
+        filtered = resources;
+    } else {
+        filtered = resources.filter(
+            item => item.category === category
+        );
+    }
+
+
+    filtered.forEach(item => {
+
+        resourceList.innerHTML += `
+
+        <div class="resource-card">
+
+            <h3>${item.title}</h3>
+
+            <p>${item.category}</p>
+
+            <a href="${item.file}" download>
+            ⬇ Download
+            </a>
+
+        </div>
+
+        `;
+
+    });
+
+}
+
+
+categoryButtons.forEach(button => {
+
+    button.addEventListener("click", ()=>{
+
+        categoryButtons.forEach(btn=>{
+            btn.classList.remove("active");
+        });
+
+
+        button.classList.add("active");
+
+
+        displayResources(
+            button.dataset.category
+        );
+
+    });
+
+});
+
+
+displayResources();
